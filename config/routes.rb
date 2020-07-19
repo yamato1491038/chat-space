0c69ee54940e76bdf5b409eedcd2b1d8d9abcbd3
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2020-07-19 15:10:55 +0900

    Intermediate_submission

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update, :index]
+  resources :groups, only: [:new, :create, :edit, :update, :index] do
+    resources :messages, only: [:index, :create]
+  end
 end
