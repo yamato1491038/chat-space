0d848e4db4c36d79b7df15474c85ad47bb15ae40
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2020-07-26 20:36:40 +0900

    finish_commit

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module ChatSpace
     end
 
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
