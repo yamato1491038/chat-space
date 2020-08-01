2a44d617c4929688e348c338177f5b44f91237bc
yamato1491038 <67918886+yamato1491038@users.noreply.github.com>
2020-08-01 20:42:49 +0900

    final_fix

--- app/assets/javascripts/modules/user.js
+++ app/assets/javascripts/modules/user.js
@@ -2,7 +2,7 @@ $(function(){
 
   function addUser(user) {
     let html = `
-                <div class="ChatMember clearfix">
+                <div class="ChatMember">
                   <p class="ChatMember__name">${user.name}</p>
                   <div class="ChatMember__add ChatMember__button" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>
@@ -23,19 +23,22 @@ $(function(){
     let html = `
                 <div class="ChatMember">
                   <p class="ChatMember__name">${name}</p>
-                  <input name="group[user_ids][]" type="hidden" value="${id}" />
+                  <input name="group[user_ids][]" type="hidden" value="${id}" class="ChatMember__id"/>
                   <div class="ChatMember__remove ChatMember__button">削除</div>
                 </div>
                 `;
     $(".ChatMembers").append(html);
   }
 
+
+
     $("#UserSearch__field").on("keyup", function() {
       let input = $("#UserSearch__field").val();
+      
       $.ajax({
         type: "GET",
         url: '/users',
-        data: { keyword: input },
+        data: {keyword: input},
         dataType: 'json'
       })
       .done(function(users) {
@@ -53,14 +56,17 @@ $(function(){
       .fail(function() {
         alert("ユーザー検索に失敗しました");
       });
+    });
+
       $("#UserSearchResult").on("click", ".ChatMember__add", function(){
         const userName = $(this).attr("data-user-name");
         const userId = $(this).attr("data-user-id");
         $(this).parent().remove();
         addChatMember(userName, userId);
       });
+
       $(".ChatMembers").on("click", ".ChatMember__remove", function() {
         $(this).parent().remove();
       });
-    });
+
 });
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,8 +6,8 @@ class GroupsController < ApplicationController
   end
 
   def create
-    #@group = Group.new(group_params)
-    if Group.create(group_params)
+    @group = Group.new(group_params)
+    if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
